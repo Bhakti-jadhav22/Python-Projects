2f1193a2c622e30efb65b2eee2f00d5141635c20
Bhakti-jadhav22 <bhaktijadhav2206@gmail.com>
2025-10-17 22:36:42 +0530

    First Commit

---
+++ Stone_Paper_Cissor.py
@@ -0,0 +1,32 @@
+#Stone Paper Cissor game
+''' 
+'1' is for Stone
+'-1' is for Paper
+'0' is for Cissor
+'''
+
+import random
+string = input("Enter your choice: ")
+yourmove = string.capitalize()
+dictionary = {"Stone": 1, "Paper":-1, "Cissor": 0}
+reversedict = {1: "Stone", -1: "Paper", 0: "Cissor"}
+
+computermove = random.choice([1,-1,0])
+print(f"Computer move is {reversedict[computermove]}\nYour move is {yourmove}")
+
+if(computermove == dictionary[yourmove]):
+    print("Tie")
+else:
+    if(computermove == 1 and dictionary[yourmove] == -1): 
+        print("You Win!")
+    elif(computermove == 1 and dictionary[yourmove] == 0): 
+        print("You Lose!")
+    elif(computermove == -1 and dictionary[yourmove] == 1): 
+        print("You Lose!")
+    elif(computermove == -1 and dictionary[yourmove] == 0):
+        print("You Win!")
+    elif(computermove == 0 and dictionary[yourmove] == 1):
+        print("You Win!")
+    elif(computermove == 0 and dictionary[yourmove] == -1):
+        print("You Lose!")
+
